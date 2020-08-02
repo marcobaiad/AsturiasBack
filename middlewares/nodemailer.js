37cf26da632ea39c12738498cf5a84f021fcd4ea
Marco Baiad <marcobaiad@gmail.com>
2020-08-01 23:00:53 -0300

    fix nodemailer on deploy

--- middlewares/nodemailer.js
+++ middlewares/nodemailer.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer');
-const { google } = require("googleapis");
+const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 
 const oauth2Client = new OAuth2(
