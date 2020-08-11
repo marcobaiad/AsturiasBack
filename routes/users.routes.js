0733493f5d8d3bcc2f872e25b014c5991ca399fe
Martin <martinfernando-sandander@hotmail.com>
2020-08-11 08:16:52 -0300

    creacion de nuevas rutas para enviar y recibir mail para recuperar la contrasenia

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,13 +2,16 @@ const express = require('express');
 const { check } = require('express-validator')
 const authorize = require('../middlewares/authorize')
 const router = express.Router();
- 
+
 const usuarioControllerRegister = require('../controllers/users/registerUsers')
 const usuarioControllerLogin = require('../controllers/users/loginUsers')
 const usuarioControllerLogout = require('../controllers/users/logoutUsers')
 const usuariosControllerReadSeveral = require('../controllers/users/readSeveralUser')
 const usuarioControllerReadOne = require('../controllers/users/readOneUser')
 const usuarioControllerDelete = require('../controllers/users/deleteUsers')
+const emailRecoveryPass = require('../controllers/users/sendrecoveryPassUser')
+const recoveryPassResponse = require('../controllers/users/recoverypassresponse')
+
 
 router.post('/login', [
     check('username', 'usuarioError: Campo Vacio.').notEmpty(),
@@ -16,7 +19,7 @@ router.post('/login', [
 ], usuarioControllerLogin.loginUser)
 router.post('/', [
     check('name', 'Campo Nombre Vacio').notEmpty(),
-    check('lastname', 'Campo Apellido Vacio').notEmpty(),
+    check('lastname', 'Campo Apellido Vació').notEmpty(),
     check('address', 'Ingresar un Mail Correcto').notEmpty(),
     check('age', 'Campo Edad Vacio').notEmpty(),
     check('phonenumber', 'Campo Celular Vacio').notEmpty(),
@@ -24,14 +27,22 @@ router.post('/', [
     check('email', 'Ingresar un Mail Correcto').isEmail(),
     check('username', 'Campo Vacio. Usuario').notEmpty(),
     check('password', ' Campo Vacio. Contraseña').notEmpty(),
-    check('password', 'la contraseña debe tener un minimo de 8 caracteres').isLength({ min: 8 }),
+    check('password', 'la contraseña debe tener un mínimo de 8 caracteres').isLength({ min: 8 }),
 ], usuarioControllerRegister.registerUser)
 
-router.get('/logout', authorize([ 'user', 'admin' ]), usuarioControllerLogout.logoutUser)
-router.get('/:id',authorize('user'), usuarioControllerReadOne.GetUser)
-router.get('/',authorize('admin'), usuariosControllerReadSeveral.GetUsers) 
+router.post('/sendrecoverypass', [
+    check('email', 'Campo Mail Vacio').notEmpty(),
+    check('email', 'Ingresar un Mail Correcto').isEmail(),
+], emailRecoveryPass.recoveyPass)
+router.put('/recoverypassresponse/:resetLink', [
+    check('newPass', ' Campo Vacio. Contraseña').notEmpty(),
+    check('newPass', 'la contraseña debe tener un mínimo de 8 caracteres').isLength({ min: 8 }),
+], recoveryPassResponse.responseRecovery)
 
-router.delete('/:id',authorize('admin'), usuarioControllerDelete.DeleteUser)
+router.get('/logout', authorize(['user', 'admin']), usuarioControllerLogout.logoutUser)
+router.get('/:id', authorize('user'), usuarioControllerReadOne.GetUser)
+router.get('/', authorize('admin'), usuariosControllerReadSeveral.GetUsers)
+
+router.delete('/:id', authorize('admin'), usuarioControllerDelete.DeleteUser)
 
 module.exports = router;
- 
